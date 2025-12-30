930c6d9e081df95558f6f9a6dddca76917db4ef8
alexdragon14 <hilmihashim472@gmail.com>
2025-12-30 22:45:46 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Achievements from "./components/Achievements";
 import Contact from "./components/Contact";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
